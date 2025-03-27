4e378970f3a0a4fd9263b4c090cbfb3aeeb8fadd
quietonmars <125283172+quietonmars@users.noreply.github.com>
2025-03-28 12:20:41 +1300

    commit2

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,13 +1,16 @@
-
 from django.urls import path
 from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
 
 from todolist.views import RegisterView, LoginView, LogoutView, UserView
 from todolist.viewsets import NoteViewSet
 
 router = routers.DefaultRouter()
 router.register('notes', NoteViewSet, basename='notes')
-urlpatterns = router.urls
-urlpatterns.append(path('register/', RegisterView.as_view(), name='register'))
-urlpatterns.append(path('login/', LoginView.as_view(), name='login'))
-urlpatterns.append(path('logout/', LogoutView.as_view(), name='logout'))
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),  # or use obtain_auth_token if you prefer
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+] + router.urls
