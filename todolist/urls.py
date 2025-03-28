d5e3fe58ad5a22a507f9274191dfbcdb17f2ddc7
quietonmars <125283172+quietonmars@users.noreply.github.com>
2025-03-28 22:54:20 +1300

    create signup function

--- todolist/urls.py
+++ todolist/urls.py
@@ -6,11 +6,11 @@ from todolist.views import RegisterView, LoginView, LogoutView, UserView
 from todolist.viewsets import NoteViewSet
 
 router = routers.DefaultRouter()
-router.register('notes', NoteViewSet, basename='notes')
+# router.register('notes', NoteViewSet, basename='notes')
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),  # or use obtain_auth_token if you prefer
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    # path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ] + router.urls
